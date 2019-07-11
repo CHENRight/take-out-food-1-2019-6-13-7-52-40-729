96fa53d2f9842f8ea2e58a08b708fde812e24e47
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 08:31:42 +0800

    add oprateBooking and test it

---
+++ src/oprateBooking.js
@@ -0,0 +1,14 @@
+
+const oprateBooking = items => {
+  let result = {}
+  items.forEach(item => {
+    item = item.replace(/\s/ig, '');
+    let temp = item.split("x");
+    result[temp[0]] = temp[1];
+  })
+  return result;
+}
+
+module.exports = {
+  oprateBooking,
+}
